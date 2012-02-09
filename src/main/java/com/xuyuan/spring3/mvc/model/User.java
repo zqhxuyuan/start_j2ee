29abe094434d3480d86bb3d82e56bd916197c53f
zqhxuyuan <zqhxuyuan@gmail.com>
2012-02-09 19:46:18 +0800

    first commit

---
+++ src/main/java/com/xuyuan/spring3/mvc/model/User.java
@@ -0,0 +1,1 @@
+package com.xuyuan.spring3.mvc.model;public class User {	private int age;	private String username;		public int getAge() {		return age;	}	public void setAge(int age) {		this.age = age;	}	public String getUsername() {		return username;	}	public void setUsername(String username) {		this.username = username;	}}
